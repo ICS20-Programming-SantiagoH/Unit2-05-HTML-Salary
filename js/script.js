e20657c0eba8d1cc2db6438b1d0074a039ab731e
santiagohewett <124379638+santiagohewett@users.noreply.github.com>
2023-03-22 15:22:11 +0000

    update to web page

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2023 Santiago Hewett All rights reserved
+//
+// Created by: Santiago Hewett
+// Created on: March 2023
+// This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates the area of the triangle.
+ */
+function calculate() {  
+  
+  const TAX_RATE = 0.2005
+  
+  // Data being collected 
+  let hoursWorked = parseFloat(document.getElementById('hours').value);
+  let rate = parseFloat(document.getElementById('rate').value);
+  let totalPay = hoursWorked * rate;
+  let tax = totalPay * TAX_RATE;
+  let takeHomeSalary = totalPay - tax;
+  
+  // Data being shown
+  document.getElementById('takeHomeSalary').innerHTML = 'Your take home salary is ' + takeHomeSalary.toFixed(2)
+  document.getElementById('taxPaid').innerHTML = 'Your tax owed is ' + tax.toFixed(2)
+}
